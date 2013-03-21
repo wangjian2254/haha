7a4c0b88e87a56d201ab945fe4d4b1add12dff45
王健 <wangjian2254@gmail.com>
2013-03-21 23:05:16 +0800

    新界面

--- haha2.py
+++ haha2.py
@@ -56,7 +56,7 @@ class listHaHa2(Page):
             if (page-1)==p:
                 nowpage='/%s/%s/%s'%(date,(p+1),limit)
             pagelist.append({'page':'/%s/%s/%s'%(date,(p+1),limit),'pagenum':p+1})
-        html=self.obj2str('templates/result2.html',{'hahalist':hahamap.get('hahalist',[]),'pagelist':pagelist,'nowpage':nowpage,'pagenum':page,'limit':limit,'nowdate':date,'perdate':perdate,'afterdate':afterdate})
+        html=self.obj2str('templates/jokeindex.html',{'hahalist':hahamap.get('hahalist',[]),'pagelist':pagelist,'nowpage':nowpage,'pagenum':page,'limit':limit,'nowdate':date,'perdate':perdate,'afterdate':afterdate})
         memcache.set('date'+date+'page'+str(page)+'limit'+str(limit),html,3600*5*page)
         self.flashhtml(html)
         return
@@ -66,7 +66,7 @@ class lookHaHa2(Page):
         html=memcache.get('joke'+jokeid)
         if not html :
             ha=Joke.get_by_key_name(jokeid)
-            html=self.obj2str('templates/joke.html',{'ha':ha,'uuid':str(uuid.uuid4()),'guest':{}})
+            html=self.obj2str('templates/jokedetail.html',{'ha':ha,'uuid':str(uuid.uuid4()),'guest':{}})
             memcache.set('joke'+jokeid,html,720000)
         self.flashhtml(html)
 class replayHaHa2(Page):
